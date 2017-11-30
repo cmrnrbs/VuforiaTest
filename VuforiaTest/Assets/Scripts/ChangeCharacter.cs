0eebfe1452500a61c65cbc5801aee1eb052fdb73
cemreonur <cemreonurb@gmail.com>
2017-12-01 00:05:34 +0300

    Demo
    Unity Demo

---
+++ VuforiaTest/Assets/Scripts/ChangeCharacter.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Unity.Editor;
+using Firebase.Database;
+
+public class User {
+	public long device_number;
+	public int level;
+	public string nickname;
+	public int hero_type;
+
+	public User() {
+	}
+
+	public User(long device_number,int level, string nickname,int hero_type) {
+		this.device_number = device_number;
+		this.level = level;
+		this.nickname = nickname;
+		this.hero_type = hero_type;
+	}
+}
+
+public class ChangeCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	public bool isPrev;
+	public bool isNext;
+	private int index;
+	private GameObject MediavalWarrior;
+	private GameObject PrevMediavalWarrior;
+	public GameObject nextButton;
+	public GameObject prevButton;
+	public GameObject Warrior;
+	public Text WarriorName;
+	DatabaseReference reference;
+	void Start () {
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://encouraging-mix-96622.firebaseio.com/");
+		reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+		//
+
+		reference
+			.GetValueAsync().ContinueWith(task => {
+				if (task.IsFaulted) {
+				}
+				else if (task.IsCompleted) {
+					DataSnapshot snapshot = task.Result;
+					if (snapshot.ChildrenCount !=0) {
+						Application.LoadLevel ("GameScene");
+					}
+				}
+			});
+
+		if (PlayerPrefs.GetInt("index",-1)==-1) {
+			PlayerPrefs.SetInt ("index", 1);
+		}
+		index = PlayerPrefs.GetInt ("index", -1);
+
+
+	}
+	public void ClickItem()
+	{
+		index = PlayerPrefs.GetInt ("index", -1);
+		int prev_index = 1;
+		if (isPrev && !isNext) {//prev button
+			if (index>1) {
+				prev_index = index;
+				index--;
+				PlayerPrefs.SetInt ("index", index);
+			}
+
+			foreach (Transform child in Warrior.transform) {
+				if (child.gameObject.name == "Hero_" + prev_index) {
+					PrevMediavalWarrior = child.gameObject;
+				}
+				else if (child.gameObject.name == "Hero_" + index) {
+					MediavalWarrior = child.gameObject;
+				}
+			}
+			MediavalWarrior.SetActive (true);
+			PrevMediavalWarrior.SetActive (false);
+			WarriorName.text = "Hero " + index;
+
+			if (index == 1) {
+				prevButton.SetActive (false);
+				nextButton.SetActive (true);
+			}
+		}
+		else if (isNext && !isPrev) {//next button
+			if (index==7) {
+				index =1;
+			}
+
+			if (index<7) {
+				prev_index = index;
+				index++;
+				PlayerPrefs.SetInt ("index", index);
+			}
+
+			foreach (Transform child in Warrior.transform) {
+				if (child.gameObject.name == "Hero_" + prev_index) {
+					PrevMediavalWarrior = child.gameObject;
+				}
+				else if (child.gameObject.name == "Hero_" + index) {
+					MediavalWarrior = child.gameObject;
+				}
+			}
+
+			MediavalWarrior.SetActive (true);
+			PrevMediavalWarrior.SetActive (false);
+			WarriorName.text = "Hero " + index;
+			if (index == 7) {
+				prevButton.SetActive (true);
+				nextButton.SetActive (false);
+			}
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpenScene()
+	{
+		int hero_type = PlayerPrefs.GetInt ("index", -1);
+		User user = new User(356938035643809,1,"CemreOnur",hero_type);
+		string json = JsonUtility.ToJson(user);
+		reference.Child("encouraging-mix-96622").Child("0").SetRawJsonValueAsync(json);
+		Application.LoadLevel ("GameScene");
+	}
+}
---
+++ VuforiaTest/Assets/Scripts/LoadCharacter.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Unity.Editor;
+using Firebase.Database;
+public class LoadCharacter : MonoBehaviour {
+
+	public class User {
+		public long device_number;
+		public int level;
+		public string nickname;
+		public int hero_type;
+
+		public User() {
+		}
+
+		public User(long device_number,int level, string nickname,int hero_type) {
+			this.device_number = device_number;
+			this.level = level;
+			this.nickname = nickname;
+			this.hero_type = hero_type;
+		}
+	}
+
+	// Use this for initialization
+	public GameObject Warrior;
+	private GameObject MyHero;
+	DatabaseReference reference;
+	void Start () {
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://encouraging-mix-96622.firebaseio.com/");
+		reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+		//
+
+		reference
+			.GetValueAsync().ContinueWith(task => {
+				if (task.IsFaulted) {
+				}
+				else if (task.IsCompleted) {
+					DataSnapshot snapshot = task.Result;
+					if (snapshot.ChildrenCount !=0) {
+						string d= "";
+					/*	long device_number = long.Parse(snapshot.Value["encouraging-mix-96622"][0]["device_number"]);
+						int hero_type = int.Parse(snapshot.Value["encouraging-mix-96622"][0]["hero_type"]);
+						string nickname = snapshot.Value["encouraging-mix-96622"][0]["nickname"];
+						int level = int.Parse(snapshot.Value["encouraging-mix-96622"][0]["level"]);*/
+						long device_number = long.Parse(string.Format("{0}", snapshot.Child("encouraging-mix-96622/0/device_number").Value));
+						int level = int.Parse(string.Format("{0}", snapshot.Child("encouraging-mix-96622/0/level").Value));
+						int hero_type = int.Parse(string.Format("{0}", snapshot.Child("encouraging-mix-96622/0/hero_type").Value));
+						string NickName = snapshot.Child("encouraging-mix-96622/0/nickname").Value.ToString();
+						string d2 = "";
+
+						foreach (Transform child in Warrior.transform) {
+							if (child.gameObject.name == "Hero_" + hero_type) {
+								MyHero = child.gameObject;
+								//child.gameObject.transform.GetChild(3).GetComponent<Text>().text = NickName;
+							
+							}
+						}
+
+
+						MyHero.gameObject.SetActive(true);
+						Component[] hingeJoints;
+
+						hingeJoints = MyHero.GetComponentsInChildren(typeof(TextMesh));
+
+						if (hingeJoints != null)
+						{
+							foreach (TextMesh joint in hingeJoints)
+								joint.text =NickName;
+						}
+					}
+				}
+			});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
